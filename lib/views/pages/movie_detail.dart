2ce384378e73bcff02ccd72aba8b46fc48646351
Nghi Tran H <nghi.tran@asiantech.vn>
2020-10-07 12:02:05 +0700

    add detail

--- lib/views/pages/movie_detail.dart
+++ lib/views/pages/movie_detail.dart
@@ -1,21 +1,155 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_driving/models/movie.dart';
+import 'package:flutter_driving/utils/constants/index.dart';
+import 'package:flutter_driving/views/widgets/movie_detail/cast_list.dart';
 
 class MovieDetailPage extends StatelessWidget {
+  final Movie movie;
+  MovieDetailPage({this.movie});
   @override
   Widget build(BuildContext context) {
+    Movie _movie = Movie(
+      id: 12321312312,
+      title: 'Joker',
+      overview:
+          'The film also features insights from commentators and thinkers on the wider implications and importance of this understanding for society, and our relationship to the environment.',
+      backdropPath:
+          'https://upload.wikimedia.org/wikipedia/en/4/45/Bloodshot_-_official_film_poster.jpeg',
+    );
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-        title: Text('Detail'),
-        centerTitle: true,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      child: Image.network(
+                        _movie.backdropPath,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      top: 0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                            colors: [
+                              Colors.black.withOpacity(0.6),
+                              Colors.transparent,
+                            ],
+                            tileMode: TileMode.repeated,
+                            stops: [
+                              0.2,
+                              0.4,
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: HeaderText(
+                              text: _movie.title,
+                            ),
+                          ),
+                          Container(
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.favorite_outline,
+                                  color: AppColors.dark_red,
+                                  size: 18,
+                                ),
+                                SizedBox(width: 5),
+                                Text(
+                                  '1245',
+                                  style: TextStyle(color: Colors.white60),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      HeaderText(
+                        text: 'Storyline',
+                      ),
+                      Text(
+                        _movie.overview,
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 14,
+                        ),
+                      ),
+                      HeaderText(
+                        text: 'Cast & Crue',
+                      ),
+                      CastList()
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            top: 30,
+            left: 20,
+            child: InkWell(
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.grey[900],
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                ),
+              ),
+              onTap: () => Navigator.of(context).pop(),
+            ),
+          ),
+        ],
       ),
-      body: Container(
-        child: Center(
-          child: Text('Detail'),
+    );
+  }
+}
+
+class HeaderText extends StatelessWidget {
+  final String text;
+  const HeaderText({
+    Key key,
+    @required String text,
+  })  : text = text,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
         ),
+        overflow: TextOverflow.ellipsis,
       ),
     );
   }
---
+++ lib/views/widgets/movie_detail/cast_list.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CastList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 250,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (_, i) => Container(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image(
+                      image: NetworkImage(
+                          'https://upload.wikimedia.org/wikipedia/en/4/45/Bloodshot_-_official_film_poster.jpeg'),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Text(
+                  'Hoang Quan',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
