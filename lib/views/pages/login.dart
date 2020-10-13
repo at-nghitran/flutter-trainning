525c01f35eee6f26c1fe29f3ad46c4db86194a7a
Nghi Tran H <nghi.tran@asiantech.vn>
2020-10-13 11:28:10 +0700

    remove Obx

--- lib/views/pages/login.dart
+++ lib/views/pages/login.dart
@@ -11,10 +11,9 @@ import 'package:flutter_driving/views/widgets/commons/custom_texfield.dart';
 import 'package:flutter_driving/views/widgets/login/header.dart';
 
 class LoginPage extends StatelessWidget {
-  final LoginController _loginController = Get.put(LoginController());
-
   @override
   Widget build(BuildContext context) {
+    final LoginController _loginController = Get.put(LoginController());
     final orientation = MediaQuery.of(context).orientation;
     return Scaffold(
       backgroundColor: Colors.white,
@@ -25,65 +24,63 @@ class LoginPage extends StatelessWidget {
             if (orientation == Orientation.portrait) SizedBox(height: 50),
             Container(
               padding: const EdgeInsets.all(Numbers.appPadding),
-              child: Obx(
-                () => Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CustomTextField(
-                      controller: _loginController.emailController,
-                      text: strings.email,
-                      icon: Icons.email,
-                      onChange: (value) {
-                        _loginController.onEmailChange();
-                      },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomTextField(
+                    controller: _loginController.emailController,
+                    text: strings.email,
+                    icon: Icons.email,
+                    onChange: (value) {
+                      _loginController.onEmailChange();
+                    },
+                  ),
+                  SizedBox(height: 30),
+                  CustomTextField(
+                    controller: _loginController.passwordController,
+                    text: strings.password,
+                    icon: Icons.lock,
+                    obscureText: true,
+                    onChange: (value) {
+                      _loginController.onPasswordChange();
+                    },
+                  ),
+                  SizedBox(height: 30),
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      color: _loginController.isDisabled.value
+                          ? Colors.white
+                          : AppColors.dark_red,
                     ),
-                    SizedBox(height: 30),
-                    CustomTextField(
-                      controller: _loginController.passwordController,
-                      text: strings.password,
-                      icon: Icons.lock,
-                      obscureText: true,
-                      onChange: (value) {
-                        _loginController.onPasswordChange();
-                      },
-                    ),
-                    SizedBox(height: 30),
-                    Container(
-                      width: double.infinity,
-                      decoration: BoxDecoration(
+                    child: OutlineButton(
+                      padding: const EdgeInsets.symmetric(vertical: 13),
+                      textColor: Colors.black,
+                      color: Colors.blue,
+                      shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(40),
-                        color: _loginController.isDisabled.value
-                            ? Colors.white
-                            : AppColors.dark_red,
                       ),
-                      child: OutlineButton(
-                        padding: const EdgeInsets.symmetric(vertical: 13),
-                        textColor: Colors.black,
-                        color: Colors.blue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(40),
-                        ),
-                        disabledTextColor: Colors.grey,
-                        onPressed: _loginController.isDisabled.value
-                            ? null
-                            : () {
-                                FocusScope.of(context).unfocus();
-                                _loginController.onLogin();
-                              },
-                        child: Text(
-                          strings.login,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: _loginController.isDisabled.value
-                                ? Colors.grey
-                                : Colors.white,
-                          ),
+                      disabledTextColor: Colors.grey,
+                      onPressed: _loginController.isDisabled.value
+                          ? null
+                          : () {
+                              FocusScope.of(context).unfocus();
+                              _loginController.onLogin();
+                            },
+                      child: Text(
+                        strings.login,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: _loginController.isDisabled.value
+                              ? Colors.grey
+                              : Colors.white,
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             )
           ],
